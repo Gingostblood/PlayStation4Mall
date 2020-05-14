59622b3db78dee8963b8bf64b626135cb2f24f90
lezzy <476935030@qq.com>
2020-05-15 01:50:03 +0800

    【rest】

--- layui/src/main/java/com/gingost/layui/domain/dto/req/ItemReqDto.java
+++
@@ -1,8 +0,0 @@
-package com.gingost.layui.domain.dto.req;
-
-/**
- * @author:lezzy
- * @Date:2020/4/2 10:27
- */
-public class ItemReqDto {
-}
--- layui/src/main/java/com/gingost/layui/repository/LogJpa.java
+++
@@ -1,8 +0,0 @@
-package com.gingost.layui.repository;
-
-import com.gingost.layui.domain.system.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface LogJpa extends JpaRepository<Log,Integer>, JpaSpecificationExecutor<Log> {
-}
